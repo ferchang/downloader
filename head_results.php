8a6c643d5069773e934688c99ae28bf036a3bcc7
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-21 01:11:44 +0330

    backup8

--- head_results.php
+++ head_results.php
@@ -8,6 +8,7 @@ echo '<textarea style="width: 100%; height: 70%">', htmlspecialchars($response, 
 <form action="" method=post>
 URL: <input type=text name=url value="<?php echo $url; ?>">
 HEAD request: <input type=checkbox name=head value=head>
+Range: <input type=text name=length value="0" size=7 style='text-align: center'>-<input type=text name=length value="All" size=7 style='text-align: center'>
 Response body:
 <select name=body_action>
 <option value=save>Save to file
