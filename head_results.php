0ca38a9a4eb034dd542ef0bf8bb0ce4eb7eb3960
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-21 01:21:06 +0330

    backup9

--- head_results.php
+++ head_results.php
@@ -2,6 +2,7 @@
 
 echo 'original url: ', $url;
 echo "<hr>";
+require 'check_curl_error.php';
 echo '<textarea style="width: 100%; height: 70%">', htmlspecialchars($response, ENT_QUOTES|ENT_SUBSTITUTE, 'UTF-8'), '</textarea>';
 
 ?>
