95e8e6a98fd7369e3a207c311080178429d0abd1
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-20 23:32:14 +0330

    backup7

--- curl_common.php
+++ curl_common.php
@@ -1,8 +1,9 @@
 <?php
 
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); 
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20); 
 curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_ENCODING , "");
 
 ?>
