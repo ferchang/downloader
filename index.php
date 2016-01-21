e818e96062fbbc38dbea5b9d3e42907fec90b3ae
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-21 20:26:00 +0330

    backup16

--- index.php
+++ index.php
@@ -9,6 +9,17 @@ header('Content-Type: text/html; charset=utf-8');
 
 session_start();
 
+if(isset($_POST['password'])) {
+	require 'password.php';
+	if($password===hash('sha256', $salt.$_POST['password'])) $_SESSION['auth']=true;
+	else echo '<span style="color: red">Entered password was wrong!</span><br><br>';
+}
+
+if(!isset($_SESSION['auth'])) {
+	require 'login_form.php';
+	exit;
+}
+
 if(isset($_POST['head'])) include 'process_head.php';
 else if(isset($_POST['get'])) include 'process_get.php';
 else include 'head_form.php';
---
+++ login_form.php
@@ -0,0 +1,4 @@
+<form method=post action=''>
+Password: <input type=text name=password>
+<input type=submit value=Login>
+</form>
