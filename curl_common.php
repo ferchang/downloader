dc7483b6c5540e34a9a4716365aac65d50493050
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-21 19:05:23 +0330

    backup14

--- curl_common.php
+++ curl_common.php
@@ -1,7 +1,7 @@
 <?php
 
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20); 
-curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt($ch, CURLOPT_TIMEOUT, 60*3);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_ENCODING , "");
