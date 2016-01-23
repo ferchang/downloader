5b08a0b28a48241bb16804ad4251edecdb63c6b2
unknown <hm@hm-d55ae99c4d55.(none)>
2016-01-23 22:18:04 +0330

    backup23

--- form.php
+++ form.php
@@ -1,12 +1,25 @@
+<script src='jquery.js'></script>
+<script>
+function head_click(stat) {
+	//alert(stat);
+	if(stat) $('#response_body_option').hide();
+	else $('#response_body_option').show();
+}
+</script>
 <form action="" method=post>
 URL: <input type=text name=url value="<?php if(isset($url)) echo $url; ?>">
-HEAD request: <input type=checkbox name=head value=head <?php if(!isset($_POST['head'])) echo 'checked'; ?>>
+HEAD request: <input type=checkbox name=head id=head_chk value=head <?php if(!isset($_POST['head'])) echo 'checked'; ?> onchange='head_click(this.checked);'>
 Range: <input type=text name=range_from value="0" size=7 style='text-align: center'>-<input type=text name=range_to value="All" size=7 style='text-align: center'>
+<span id=response_body_option>
 Response body:
 <select name=body_action>
 <option value=save>Save to file
 <option value=show>Show
 <option value=show8save>Show and save
 </select>
+</span>
 <input type=submit value=Submit name=get>
 </form>
+<script>
+if($('#head_chk').is(':checked')) $('#response_body_option').hide();
+</script>
