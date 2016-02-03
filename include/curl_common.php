0aa3df64cfddd42d25046db268304f7ff9353fb9
ferchange <hamidreza_mz@yahoo.com>
2016-02-03 12:32:27 +0330

    backup45

--- include/curl_common.php
+++ include/curl_common.php
@@ -7,7 +7,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_ENCODING , "");
 
 if(isset($_SESSION['proxy'])) {
-	$proxy=$_SESSION['proxy']['host'].':'.$_SESSION['proxy']['port'];
+	if(stripos($url, 'https:')===0 and isset($_SESSION['proxy']['https_port'])) $proxy=$_SESSION['proxy']['host'].':'.$_SESSION['proxy']['https_port'];
+	else $proxy=$_SESSION['proxy']['host'].':'.$_SESSION['proxy']['port'];
+	//echo "<br>proxy: $proxy<br>";
 	curl_setopt($ch, CURLOPT_PROXY, $proxy);
 	if($_SESSION['proxy']['kind']==='socks') curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 }
