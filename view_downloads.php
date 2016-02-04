ede08d0596f0435698ad2d2ba9840ed781bc6ea6
unknown <hm@hm-d55ae99c4d55.(none)>
2016-02-04 20:43:15 +0330

    backup53

---
+++ view_downloads.php
@@ -0,0 +1,11 @@
+<?php
+if(ini_get('register_globals')) exit("<center><h3>Error: Turn that damned register globals off!</h3></center>");
+define('CAN_INCLUDE', true);
+
+require 'include/common.php';
+
+require ROOT.'include/auth.php';
+
+
+
+?>
