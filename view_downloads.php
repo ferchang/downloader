a41b0defd05a52e380cbd7901fbf3e0e2899b94a
unknown <hm@hm-d55ae99c4d55.(none)>
2016-02-04 21:12:25 +0330

    backup54

--- view_downloads.php
+++ view_downloads.php
@@ -6,6 +6,19 @@ require 'include/common.php';
 
 require ROOT.'include/auth.php';
 
+$arr=glob('downloads/*.info');
 
+echo '<table border align=center cellpadding=10>';
+
+foreach($arr as $name) {
+	$name=substr($name, 10, -5);
+	echo '<tr><td>';
+	echo "<a href=downloads/$name>$name</a>";
+	echo '</td><td>';
+	echo '<textarea cols=50 rows=5>', file_get_contents("downloads/$name.info"), '</textarea>';
+	echo '</td></tr>';
+}
+
+echo '</table>';
 
 ?>
